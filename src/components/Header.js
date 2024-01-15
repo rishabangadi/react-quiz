143a22b3d9b10e6939553dd9e5bb9ab855a2f75a
Rishab Angadi <rishab@Rishabs-MacBook-Air.local>
2024-01-15 13:58:53 +0530

    Updated Readme.md

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <header className="app-header">
+      <img src="logo.png" alt="React logo" />
+      <h1>The FrontEnd Quiz</h1>
+    </header>
+  );
+}
+
+export default Header;
